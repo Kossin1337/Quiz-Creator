3317a8b57e63aa4708d6a76e503d05e9a6e4de8f
Kossin1337 <piotrswobodzinski@gmail.com>
2021-10-09 23:03:35 +0200

    Landing page creation

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const LandingPage = () => {
+  return <div className="langing-page"></div>;
+};
+
+// {
+//   quizID: 'xd1ars',
+//   title: 'quiz title',
+//   questions: [
+//     {
+//       id: 1,
+//       questionTitle: "Four (4) in Spanish is:",
+//       answers: ['Uno', 'Dos', 'tres', 'cuatro'],
+//       correctAnswerID: 3
+//     }
+//     {
+//       id: 2,
+//       questionTitle: "Two (2) in Spanish is:",
+//       answers: ['Uno', 'Dos', 'tres', 'cuatro'],
+//       correctAnswerID: 1
+//     }
+//   ]
+// }
