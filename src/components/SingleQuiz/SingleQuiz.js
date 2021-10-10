25c743058708cf6f585cb50a5e997fa56938ee61
Kossin1337 <piotrswobodzinski@gmail.com>
2021-10-10 12:06:11 +0200

    Single Quiz Section update

--- src/components/SingleQuiz/SingleQuiz.js
+++ src/components/SingleQuiz/SingleQuiz.js
@@ -3,11 +3,13 @@ import { useParams } from "react-router";
 import firebase from "../../utils/firebase";
 
 import "./style.scss";
+
 export const SingleQuiz = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [questionIndex, setQuestionIndex] = useState(0);
 
-  const { quizid } = useParams()
+  const { quizid } = useParams();
 
   const ref = firebase.firestore().collection("quiz").doc(quizid);
 
@@ -23,31 +25,77 @@ export const SingleQuiz = () => {
     // eslint-disable-next-line
   }, []);
 
-
   if (loading) {
     return <h1>Loading ...</h1>;
   }
 
+  console.log(`Single quiz data`);
+  console.log(data);
+
+  function prevQuestion() {
+    if (questionIndex !== 0) {
+      setQuestionIndex((prevIndex) => prevIndex - 1);
+    }
+  }
+
+  function nextQuestion() {
+    if (questionIndex < data.numberOfQues) {
+      setQuestionIndex((prevIndex) => prevIndex + 1);
+    }
+  }
+
   return (
     <div className="single-quiz">
-
-      <div className="quiz-section" >
-        <h1>{data.quizName}</h1>
-        <div>
-          {data.quizQues && data.quizQues.map((item, i) => (
-            <div className="quiz-question" key={i}>
-              <h2>{item.questionText}</h2>
-              <ul>
-                {item.answerOptions.map((option, index) => (
-                  <li key={index} >{option}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-        <button>Submit</button>
+      <div className="quiz-section">
+        <h1 className="quiz-title">{data.quizName}</h1>
+        <h2 className="question-number-indicator">
+          Question {questionIndex + 1} of {data.numberOfQues}
+        </h2>
+        {/* <div className="quiz-content">
+          <div className="quiz-question">
+            <h2>{data.quizQues[questionIndex].questionText}</h2>
+            <ul className="answers">
+              {data.quizQues[questionIndex].answerOptions.map(
+                (option, index) => (
+                  <li className="answer" key={index}>
+                    {option}
+                  </li>
+                )
+              )}
+            </ul>
+          </div>
+        </div> */}
+        {questionIndex === data.numberOfQues - 1 ? (
+          <button className="submit-btn">Submit</button>
+        ) : (
+          <div className="buttons">
+            <button
+              className="navigation-btn last-question"
+              onClick={prevQuestion}
+            >
+              <i class="fas fa-angle-left"></i>
+            </button>
+            <button
+              className="navigation-btn next-question"
+              onClick={nextQuestion}
+            >
+              <i class="fas fa-angle-right"></i>
+            </button>
+          </div>
+        )}
       </div>
-
     </div>
   );
 };
+
+/* {data.quizQues &&
+            data.quizQues.map((item, i) => (
+              <div className="quiz-question" key={i}>
+                <h2>{item.questionText}</h2>
+                <ul>
+                  {item.answerOptions.map((option, index) => (
+                    <li key={index}>{option}</li>
+                  ))}
+                </ul>
+              </div>
+            ))} */
